b2a72ab5892aa8b419bd731288bb935aa631617a
wangmeiyue <3060494927@qq.com>
2019-02-14 20:00:00 -1200

    swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,18 @@
-var dl = document.querySelectorAll("dl")
-dl.forEach(function(file) {
-    file.onclick = function() {
-        console.log(this)
+var dl = Array.from(document.querySelectorAll("dl"))
+var swiperr = new Swiper(".swiper-container", {
+    on: {
+        slideChange: function() {
+            dl.forEach(function(file) {
+                file.classList.remove("active")
+            })
+            dl[this.activeIndex].classList.add("active")
+        }
+    }
+})
+dl.forEach(function(file, ind) {
+    file.index = ind
+    file.ontouchend = function(e) {
+        e.preventDefault()
+        swiperr.slideTo(ind)
     }
 })

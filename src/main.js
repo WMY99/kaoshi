68bf287717fa6054f2f9d0038faadd7b03aef496
wangmeiyue <3060494927@qq.com>
2019-02-19 09:03:55 -1200

    第二周第一天

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 王美悦 
+ * @Date: 2019-02-19 08:45:32 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-02-19 08:57:19
+ */
+require.config({
+    baseUrl: "./js",
+    paths: {
+        "a": "a",
+        "zepot": "../lib/zepot"
+    }
+})
